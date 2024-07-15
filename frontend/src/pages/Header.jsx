ad9b591c4f780c3360f5c9d15aef5f2f533851f9
57ajay.u <57ajay.u@gmail.com>
2024-07-15 13:26:44 +0000

    developing backend

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -5,10 +5,11 @@ const Header = ()=>{
         <header className="bg-stone-300 shadow-md">
             <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
             <Link to="/">
-                <h1 className="text-sm font-bold sm:text-xl flex flex-wrap">
-                    <span className="text-slate-700">Home</span>
-                    <span className="text-slate-950">Findr</span>
-                </h1>
+            <h1 class="text-sm font-bold sm:text-xl flex flex-wrap transform transition-transform hover:scale-110">
+                <span class="text-slate-700">Home</span>
+                <span class="text-slate-950">Findr</span>
+            </h1>
+
             </Link>
             <form className="bg-slate-100 p-3 rounded-lg flex items-center">
                 <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
