a59ce1605d6abaa18370fc921b3460113753e070
57ajay.u <57ajay.u@gmail.com>
2024-07-16 14:01:58 +0000

    hashing password before saving it to the database

--- backend/models/user.schema.js
+++ backend/models/user.schema.js
@@ -1,4 +1,6 @@
 import { Schema, model } from "mongoose"; 
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema({
     username: {
@@ -17,5 +19,23 @@ const userSchema = new Schema({
         unique: true,
     }
 }, {timestamps: true});
+
+userSchema.pre('save', async(next)=>{
+    if (this.isModified('password')){
+        const salt = bcrypt.genSaltSync(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    };
+});
+
+userSchema.methods.isPasswordMatched = async(password)=>{
+    if(this.password){
+        return await bcrypt.compare(password, this.password);
+    }
+    else{
+        return false
+    };
+};
+
+
 
 export const User = model('User', userSchema);
