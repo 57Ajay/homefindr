96ee635ea94e63b718aa14c602e333e342a6dfd0
57Ajay <57ajay.u@gmail.com>
2024-07-19 01:35:31 +0530

    minor bug

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -19,7 +19,7 @@ const Header = ()=>{
                 <FaSearch className="" />
             </form>
             <ul className="flex gap-4">
-                <Link to="/home">
+                <Link to="/">
                     <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">Home</li>
                 </Link>
                 <Link to="/about">
